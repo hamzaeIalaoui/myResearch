1c5ba9788c378e86801940e21895bfba21ba20d2
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-03-28 16:47:10 +0000

    added the models file where all the definition of objects in the database will be created and the index.js file that will control the rest requests using express

---
+++ server/index.js
@@ -0,0 +1,27 @@
+//importing the librairies we need
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+
+const app=express();
+
+
+//implimentation of the routes
+
+
+
+
+
+
+
+
+
+
+
+
+//runung the server
+app.listen(3001,()=>{
+    console.log('server is running on port 3001');
+}
+);
