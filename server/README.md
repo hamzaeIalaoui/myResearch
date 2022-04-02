688d7daa85f99d67fee1f16badd320709327a011
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-04-02 17:00:33 +0000

    created an oauth system for our api

---
+++ server/README.md
@@ -0,0 +1,3 @@
+for the structure of the project api it is as follows 
+it is using two server one for api oauth, one for recieving post and get requests safely 
+to post or get request follow the request guide in the file requests.rest (download extension rest client to test them)
--- server/index.js
+++ server/index.js
@@ -1,28 +1,29 @@
-//importing the librairies we need
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+app.get('/test',authenticateToken,(req,res)=>{
+    res.send('Hello World')
+    })
 
 
 
-const app=express();
-app.use(cors());
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
 
-
-
-//implimentation of the routes
-
-
-
-
-
-
-
-
-
-
-
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
 
 //running the server
 app.listen(3001,()=>{
