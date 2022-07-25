a34cf6bb33cf51b51ced36c3ce3bab58788ddbc1
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-07-25 03:25:49 +0100

    base server config

---
+++ server/authServer.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const bycrypt = require('bcrypt')
+const {MongoClient} = require('mongodb')
+
+app.use(express.json())
+//refresh token for now are stored locally, in the future they will be stored in the database
+let refreshTokens = []
+
+
+const client= new MongoClient(process.env.MONGODB_URI)
+client.connect(err => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('Connected to database')
+}
+)
+
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
+})
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
+
+app.post('/login', async (req, res) => {
+  const email = req.body.email
+  const password= req.body.password
+  const user = { name: email}
+  try{
+    const result=await client.db("myResearch").collection("admin").findOne({email:email})
+
+    if(result==null){
+      res.sendStatus(401).json({error:'Invalid email or password'})
+    }
+    else{
+      if(await bycrypt.compare(password,result.password)){
+        const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({ accessToken: accessToken, refreshToken: refreshToken })
+      }
+      else{
+        res.sendStatus(401).json({error:'Invalid email or password'})
+      }
+      
+    }
+
+  }
+  catch(err){
+    console.log(err)
+  }
+   
+
+  
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3600s' })
+}
+
+app.listen(4000)
---
+++ server/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const {MongoClient} = require('mongodb')
+
+
+app.use(express.json())
+const client= new MongoClient(process.env.MONGODB_URI)
+client.connect(err => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('Connected to database')
+}
+)
+
+app.get('/test', authenticateToken, (req, res) => {
+    res.status(200).json({
+        message: 'Authenticated'
+    })
+})
+
+
+
+
+
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      console.log(err)
+      if (err) return res.sendStatus(403)
+      req.user = user
+      next()
+    })
+  }
+
+  app.listen(3001,()=>{
+    console.log('server is running on port 3001');
+}
+);
+  
